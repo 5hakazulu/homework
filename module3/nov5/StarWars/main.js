33851e1ee4c0d43db7edafa1601474442b922235
Joshua Zuluaga <zazuofcloud10@gmail.com>
2022-11-08 22:18:24 -0500

    worked on starwars

--- module3/nov5/StarWars/main.js
+++ module3/nov5/StarWars/main.js
@@ -1,61 +1,107 @@
 // https://swapi.dev/api/
 
-const myForm = document.getElementById("searchForm");
-const subjectDropDownMenu = document.getElementById('subjectSelector');
-let websiteName = '';
-const subjectCardTemplate = document.querySelector("[data-subject-template]")
+let arrayOfPeople= [];
 
+// async function fetchPeople(){
+    
+//     const result = await fetch("https://swapi.dev/api/people");
+//     const resultJson = await result.json();
+//     console.log(resultJson);
+// }
 
-myForm.addEventListener("submit",function(e){
-    e.preventDefault();
-    // console.log("hi")
-    const searchString = document.getElementById('searchBar').value
-    const urlEncodedSearchString =encodeURIComponent(searchString);
-    fetch(websiteName +=urlEncodedSearchString)
-        .then(function(response){
-            return response.json();
-        })
-        // .then(function(data){
+async function fetchPeople(){
+    let next = true;
+    let page = 1;
+    while(next){
+        const result = await fetch(`https://swapi.dev/api/people/?page=${page}`);
+        const resultJson = await result.json();
 
-        // })
-    console.log(response.json);
-    // if(){
-
-    // }
-})
-
-// document.getElementById("searchButton").addEventListener('button', function(){
-//     console.log("hi");
-// })
-
-function getOption(){
-    const selectElement = document.querySelector('#subjectSelector')
-    return selectElement.value;
+        arrayOfPeople =[...arrayOfPeople, ...resultJson.results];
+        // for (let i= 0; i< resultJson.results.length; i++){
+        //     arrayOfPeople.push(resultJson.results[i])
+        // }
+      
+        next = resultJson.next;
+        page++;
+    }
+    console.log(arrayOfPeople);
+    renderName(arrayOfPeople);
+   
 }
 
-subjectDropDownMenu.addEventListener('change', (event)=>{
-    websiteName =`https://swapi.dev/api/${getOption()}/`;
-    console.log(websiteName);
+function renderName(arrayOfResults){
+    console.log('hi')
+    const nameHTMLArray = arrayOfResults.map(function(result){
+        
+        return `
+        <p>${result.name}</p>
+        `
+    })
+    const nameHTML = nameHTMLArray.join('');
+    const nameDiv =document.getElementById("people");
+    nameDiv.innerHTML = nameHTML;
+}
+
+fetchPeople();
+// console.log(arrayOfPeople);
+
+
+// const myForm = document.getElementById("searchForm");
+// const subjectDropDownMenu = document.getElementById('subjectSelector');
+// let websiteName = '';
+// const subjectCardTemplate = document.querySelector("[data-subject-template]")
+
+
+// myForm.addEventListener("submit",function(e){
+//     e.preventDefault();
+//     // console.log("hi")
+//     const searchString = document.getElementById('searchBar').value
+//     const urlEncodedSearchString =encodeURIComponent(searchString);
+//     fetch(websiteName +=urlEncodedSearchString)
+//         .then(function(response){
+//             return response.json();
+//         })
+//         // .then(function(data){
+
+//         // })
+//     console.log(response.json);
+//     // if(){
+
+//     // }
+// })
+
+// // document.getElementById("searchButton").addEventListener('button', function(){
+// //     console.log("hi");
+// // })
+
+// function getOption(){
+//     const selectElement = document.querySelector('#subjectSelector')
+//     return selectElement.value;
+// }
+
+// subjectDropDownMenu.addEventListener('change', (event)=>{
+//     websiteName =`https://swapi.dev/api/${getOption()}/`;
+//     console.log(websiteName);
     
-    fetch(websiteName)
-        .then(res=> res.json())
+//     fetch(websiteName)
+//         .then(res=> res.json())
        
-        .then(data=> {
+//         .then(data=> {
           
-            console.log(data);
-            data.results.forEach(element => {
-                const card = subjectCardTemplate.content.cloneNode(true).children[0]
-                if(getOption() === "films"){
-                    console.log(element.title)
-                }
-                else{
-                    console.log(element.name);
-                }
+//             console.log(data);
+//             data.results.forEach(element => {
+//                 const card = subjectCardTemplate.content.cloneNode(true).children[0]
+//                 if(getOption() === "films"){
+//                     console.log(element.title)
+//                 }
+//                 else{
+//                     console.log(element.name);
+//                 }
                 
             
-            });
+//             });
             
-        })
-    // return websiteName;
-})
+//         })
+//     // return websiteName;
+// })
 
